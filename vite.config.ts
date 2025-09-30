11f8c4458f2e41864b2892e87b4ff6848c846162
AnujBag012 <anujsbag@gmail.com>
2025-09-30 09:13:12 +0530

    change-1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  // base: "/Comfort-Compass/",  // 
+  base: "/Comfort-Compass/",  // 
   server: {
     host: "::",
     port: 8080,
